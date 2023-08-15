f2926e75ce4290da54dc896d18c916ce048d24c4
CleuJunior <cleu.junior@gmail.com>
2023-08-15 18:31:19 -0300

    feat: inital commit

---
+++ src/main/java/com/catchemall/CatchEmAllApplication.java
@@ -0,0 +1,13 @@
+package com.catchemall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatchEmAllApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatchEmAllApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/catchemall/CatchEmAllApplicationTests.java
@@ -0,0 +1,13 @@
+package com.catchemall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatchEmAllApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
